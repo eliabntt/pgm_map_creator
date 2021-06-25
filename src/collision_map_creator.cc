d5b5229e0111146ed2ba575ef5bdb37ad1e96d40
Elia Bonetto <eliabntt94@gmail.com>
2021-06-25 11:50:55 +0200

    compiling with newer gazebo

--- src/collision_map_creator.cc
+++ src/collision_map_creator.cc
@@ -1,3 +1,4 @@
+#define int_p_NULL (int*)NULL
 #include <fstream>
 #include <iostream>
 #include <math.h>
@@ -19,30 +20,26 @@ namespace gazebo
 typedef const boost::shared_ptr<
   const collision_map_creator_msgs::msgs::CollisionMapRequest>
     CollisionMapRequestPtr;
-
 class CollisionMapCreator : public WorldPlugin
 {
   transport::NodePtr node;
   transport::PublisherPtr imagePub;
   transport::SubscriberPtr commandSubscriber;
   physics::WorldPtr world;
-
   public: void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
   {
     node = transport::NodePtr(new transport::Node());
     world = _parent;
     // Initialize the node with the world name
-    node->Init(world->GetName());
+    node->Init(world->Name());
     std::cout << "Subscribing to: " << "~/collision_map/command" << std::endl;
     commandSubscriber = node->Subscribe("~/collision_map/command",
       &CollisionMapCreator::create, this);
     imagePub = node->Advertise<msgs::Image>("~/collision_map/image");
   }
-
   public: void create(CollisionMapRequestPtr &msg)
   {
     std::cout << "Received message" << std::endl;
-
     std::cout << "Creating collision map with corners at (" <<
       msg->upperleft().x() << ", " << msg->upperleft().y() << "), (" <<
       msg->upperright().x() << ", " << msg->upperright().y() << "), (" <<
@@ -52,24 +49,20 @@ class CollisionMapCreator : public WorldPlugin
       msg->height() << "\nResolution = " << msg->resolution() << " m\n" <<
         "Occupied spaces will be filled with: " << msg->threshold() <<
         std::endl;
-
     double dX_vertical = msg->upperleft().x() - msg->lowerleft().x();
     double dY_vertical = msg->upperleft().y() - msg->lowerleft().y();
     double mag_vertical =
       sqrt(dX_vertical * dX_vertical + dY_vertical * dY_vertical);
     dX_vertical = msg->resolution() * dX_vertical / mag_vertical;
     dY_vertical = msg->resolution() * dY_vertical / mag_vertical;
-
     double dX_horizontal = msg->upperright().x() - msg->upperleft().x();
     double dY_horizontal = msg->upperright().y() - msg->upperleft().y();
     double mag_horizontal =
       sqrt(dX_horizontal * dX_horizontal + dY_horizontal * dY_horizontal);
     dX_horizontal = msg->resolution() * dX_horizontal / mag_horizontal;
     dY_horizontal = msg->resolution() * dY_horizontal / mag_horizontal;
-
     int count_vertical = mag_vertical / msg->resolution();
     int count_horizontal = mag_horizontal / msg->resolution();
-
     if (count_vertical == 0 || count_horizontal == 0)
     {
       std::cout << "Image has a zero dimensions, check coordinates"
@@ -77,18 +70,16 @@ class CollisionMapCreator : public WorldPlugin
       return;
     }
     double x,y;
-
     boost::gil::gray8_pixel_t fill(255-msg->threshold());
     boost::gil::gray8_pixel_t blank(255);
     boost::gil::gray8_image_t image(count_horizontal, count_vertical);
-
     double dist;
     std::string entityName;
     ignition::math::Vector3d start, end;
     start.Z(msg->height());
-    end.Z(0.001);
+    end.Z(0.15);
 
-    gazebo::physics::PhysicsEnginePtr engine = world->GetPhysicsEngine();
+    gazebo::physics::PhysicsEnginePtr engine = world->Physics();
     engine->InitForThread();
     gazebo::physics::RayShapePtr ray =
       boost::dynamic_pointer_cast<gazebo::physics::RayShape>(
